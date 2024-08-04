4868ce89b0e7ac7a70ea86271065d8afd34428bd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-08-04 15:59:21 +0500

    Add: working header now

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,20 @@
-// import {getSignInUrl, getUser, signOut} from "@workos-inc/authkit-nextjs";
+import {getSignInUrl, getUser, signOut} from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
 
 export default async function Header() {
-//   const { user } = await getUser();
-//   const signInUrl = await getSignInUrl();
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
   return (
     <header>
       <div className="container flex items-center justify-between mx-auto my-4">
         <Link href={'/'} className="font-bold text-xl">Job Board</Link>
         <nav className="flex gap-2">
-          {/* {!user && (
+          {!user && (
             <Link className="rounded-md bg-gray-200 py-1 px-2 sm:py-2 sm:px-4" href={signInUrl}>
               Login
             </Link>
-          )} */}
-          {/* {user && (
+          )}
+          {user && (
             <form action={async () => {
               'use server';
               await signOut();
@@ -23,7 +23,7 @@ export default async function Header() {
                 Logout
               </button>
             </form>
-          )} */}
+          )}
           <Link className="rounded-md py-1 px-2 sm:py-2 sm:px-4 bg-blue-600 text-white" href={'/new-listing'}>
             Post a job
           </Link>
